fa063a3ada7caa896e955f3f919714836d2692b5
Remy Dufay <remy.dufay@gmail.com>
2022-06-21 16:51:26 +0200

    Mise en forme index rapide

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Chaussure.destroy_all
+User.destroy_all
+
+
+
+puts "Creation user"
+user = User.create(email: "blabla@gmail.com", password: "azertyu")
+
+puts "Creation chaussure"
+Chaussure.create(name: "Nike" , user: user, size: 41 )
+Chaussure.create(name: "Adidas" , user: user, size: 20 )
+Chaussure.create(name: "Chouchou" , user: user, size: 41 )
+Chaussure.create(name: "Santi" , user: user, size: 41 )
+Chaussure.create(name: "Claquette" , user: user, size: 21 )
+Chaussure.create(name: "Chaussette" , user: user, size: 43 )
+Chaussure.create(name: "Vans" , user: user, size: 41 )
+Chaussure.create(name: "Lascar" , user: user, size: 31 )
+Chaussure.create(name: "Bim" , user: user, size: 41 )
+Chaussure.create(name: "Pierrot" , user: user, size: 40 )
+
+
+puts "Done !"
